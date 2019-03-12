145b4caa1f1287e4159470f6a4645cf4c2c9aae2
unknown <louise.w1997@gmail.com>
2019-03-12 18:58:10 -0500

    modified hpi_io_intf

--- lab8/hpi_io_intf.sv
+++ lab8/hpi_io_intf.sv
@@ -18,28 +18,30 @@ always_ff @ (posedge Clk)
 begin
     if(Reset)
     begin
-        from_sw_data_out_buffer <= 
-        OTG_ADDR                <= 
-        OTG_RD_N                <= 
-        OTG_WR_N                <= 
-        OTG_CS_N                <= 
-        OTG_RST_N               <= 
-        from_sw_data_in         <= 
+        from_sw_data_out_buffer <= 16'b0;
+        OTG_ADDR                <= 16'b0;
+        OTG_RD_N                <= 1;
+        OTG_WR_N                <= 1;
+        OTG_CS_N                <= 0;
+        OTG_RST_N               <= 1;
+        from_sw_data_in         <= 16'b0;
     end
     else 
     begin
-        from_sw_data_out_buffer <= 
-        OTG_ADDR                <= 
-        OTG_RD_N                <= 
-        OTG_WR_N                <= 
-        OTG_CS_N                <= 
-        OTG_RST_N               <= 
-        from_sw_data_in         <= 
+        from_sw_data_out_buffer <= from_sw_data_out;
+        OTG_ADDR                <= from_sw_address;
+        OTG_RD_N                <= from_sw_r;
+        OTG_WR_N                <= from_sw_w;
+        OTG_CS_N                <= from_sw_cs;
+        OTG_RST_N               <= from_sw_reset;
+        from_sw_data_in         <= from_sw_data_out_buffer;
     end
 end
 
 // OTG_DATA should be high Z (tristated) when NIOS is not writing to OTG_DATA inout bus.
 // Look at tristate.sv in lab 6 for an example.
-assign OTG_DATA = 
+
+//need to be changed
+assign OTG_DATA = (check if writing to OTg) ? from_sw_data_out_buffer : {16{1'bZ}};
 
 endmodule 
